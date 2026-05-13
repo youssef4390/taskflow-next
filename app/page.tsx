54ba8d45570c63ddcc6e25e061f5015ef0ddb845
Youssef4390 <youssef4390@gmail.com>
2026-05-13 20:46:18 +0100

    TP 01 - Next JS - Du CSR au SSR

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+ return (
+ <div style={{ padding: '2rem', textAlign: 'center' }}>
+ <h1>Bienvenue sur TaskFlow</h1>
+ <p>Gestion de projets collaboratifs</p>
+ <a href="/login">Se connecter</a>
+ </div>
+ );
+}
